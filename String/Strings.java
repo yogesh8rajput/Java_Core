37715021a5579f0bc77b731e967f705a786991ea
yogesh8rajput <159117998+yogesh8rajput@users.noreply.github.com>
2025-01-10 21:26:20 +0530

    Update Strings.java

--- String/Strings.java
+++ String/Strings.java
@@ -5,7 +5,7 @@ public static void main(String arg[]){
        Scanner sc= new Scanner(System.in);
 	   String name=sc.next();//single word
 	   System.out.println("Enter your name");
-	      System.out.println("Your name"+name);
+	      System.out.println("Your_name"+name);
 	  
 }
 }
