c1bddb0e6446e9b5a1eba7c54364697e95bd4d3a
yogesh8rajput <159117998+yogesh8rajput@users.noreply.github.com>
2025-01-12 20:07:15 +0530

    Update Helllo.java

--- Normal/Helllo.java
+++ Normal/Helllo.java
@@ -3,7 +3,7 @@ import java.awt.Graphics;
 
 class Hello extends Applet{
 public void paint(Graphics g){
-g.drawString("Welcome to applet.Does it work or not???",100,100);
+g.drawString("Welcome to applet.Does work or not???",100,100);
 }
 
  }
