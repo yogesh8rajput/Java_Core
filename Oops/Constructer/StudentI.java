73488f5c4f70f7a3451ed841d18ec7040e1632c3
yogesh8rajput <159117998+yogesh8rajput@users.noreply.github.com>
2025-01-27 19:28:58 +0530

    Update StudentI.java

--- Oops/Constructer/StudentI.java
+++ Oops/Constructer/StudentI.java
@@ -16,11 +16,11 @@ class Student{
   {
     Student s1=new Student();
 	s1.name="Ayush";
-	s1.age=14;
+	s1.age=15;
 	s1.stdinfo();
 	Student s2=new Student();
-	s2.name="Deep";
-	s2.age=20;
+	s2.name="Deepu";
+	s2.age=21;
 	
 	s2.stdinfo();
   }
