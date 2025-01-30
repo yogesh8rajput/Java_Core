c15f436ab63ea8a02cbae7106d2c7e00d409ba44
yogesh8rajput <yogeshmyrajput38203@gmail.com>
2025-01-30 21:58:53 +0530

    update

--- String/Strings2.java
+++ String/Strings2.java
@@ -4,7 +4,7 @@ public static void main(String arg[]){
     String name="Tony";
 	String last="Stark";	
     String fulname=name+" "+last;
-	   System.out.println("Your name:"+fulname);
+	   System.out.println("Your name :"+fulname);
 	   System.out.println("Length:"+fulname.length());
 	   //charAt single element print
 	   for(int i=0;i<fulname.length(); i++){
