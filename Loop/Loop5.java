e774b7a3edd81ec3be59e9ddc98bcd0cf679aa95
yogesh8rajput <159117998+yogesh8rajput@users.noreply.github.com>
2025-01-31 21:15:59 +0530

    Update Loop5.java

--- Loop/Loop5.java
+++ Loop/Loop5.java
@@ -4,12 +4,15 @@ class Loop5
 {
    public static void main(String arg[])
    {
+
+      
    int n,sum=0;
    Scanner sc=new Scanner(System.in);
    n=sc.nextInt();
    for( int i=1;i<=n;i++)
    {
    sum=sum+i;
+      
    }
    System.out.println(sum);
    
