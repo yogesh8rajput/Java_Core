f877cf64cd355b3afe209a2c06da3a3adcd058ad
yogesh8rajput <159117998+yogesh8rajput@users.noreply.github.com>
2025-04-26 20:44:51 +0530

    Update Product.java

--- Function/Product.java
+++ Function/Product.java
@@ -8,11 +8,12 @@ class Product
 public static void main(String arg[])
 {
     Scanner sc=new Scanner(System.in);
-	System.out.println("Enter two number");
+	System.out.println("Enter first number");
 	int a=sc.nextInt();
+	System.out.println("Enter second number");
 	int b=sc.nextInt();
 	int product=calculateProduct(a,b);
-	System.out.println("Product of two number:"+product);
+	System.out.println("Product of two numbers:"+product);
 	
 }
 }
