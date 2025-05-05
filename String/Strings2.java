63ed2c2305438cb47f516978f33e0e716a393d57
yogesh8rajput <yogeshmyrajput38203@gmail.com>
2025-05-05 21:28:39 +0530

    update string

--- String/Strings2.java
+++ String/Strings2.java
@@ -6,7 +6,7 @@ public static void main(String arg[]){
     String fulname=name+" "+last;
 	   System.out.println("Your name :"+fulname);
 	   System.out.println("Length:"+fulname.length());
-	   //charAt single element print
+	  
 	   for(int i=0;i<fulname.length(); i++){
 		   System.out.println(fulname.charAt(i));
 		   
