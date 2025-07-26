85a691fb1483d27e71cb5889782b0449ff6ddfb0
yogesh8rajput <159117998+yogesh8rajput@users.noreply.github.com>
2025-07-26 23:52:06 +0530

    Update W03_P3.java

--- W03_P3.java
+++ W03_P3.java
@@ -1,25 +1,13 @@
  import java.util.Scanner;
 class W03_P3{
- 
- 
  public static long factorial(int x)
     {
         if (x >= 1)
-		
             return x * factorial((x) -1;
-
-		
-
-            
         else
             return ;
     }
-	
-	
-	public static void main(String[] args) {
-
-
-		
+	public static void main(String[] args) {	
         Scanner in = new Scanner(System.in);
         int x;
         x = in.nextInt();
