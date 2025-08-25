c76328a25b204b19b6bbe95aed669fcc904852aa
yogesh8rajput <159117998+yogesh8rajput@users.noreply.github.com>
2025-08-25 21:26:50 +0530

    Update Hard.java

--- Latest/Hard.java
+++ Latest/Hard.java
@@ -3,6 +3,6 @@ class SHard
 {
    public static void main(String arg[])
    {
-   System.out.println("Hello Welcome to learnings Java core ,oops, data structure,Advance , Spring , Hibernate,Spring Boot,React learning classes working");
+   System.out.println("Hello Welcome to learnings Java core ,oops, data structure,Advance , Spring , Hibernate,Spring Boot,React learning class working");
    }
 }
