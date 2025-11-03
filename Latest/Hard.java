b9b2ce67a7354f6af6124b5cfc06d523e60ef5a0
yogesh8rajput <159117998+yogesh8rajput@users.noreply.github.com>
2025-11-03 22:56:01 +0530

    Update Hard.java

--- Latest/Hard.java
+++ Latest/Hard.java
@@ -3,6 +3,6 @@ class SHard
 {
    public static void main(String arg[])
    {
-   System.out.println("Welcome to new yes classes in Java to classes point be noted");
+   System.out.println("Welcome to new yes classes in Java and advance to classes point be noted");
    }
 }
