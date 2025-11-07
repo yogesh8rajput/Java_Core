3d5877e062dfbcd65cb5b102b76b3fe5802def1d
yogesh8rajput <yogeshmyrajput38203@gmail.com>
2025-11-07 20:56:36 +0530

    today

--- Latest/Hard.java
+++ Latest/Hard.java
@@ -3,6 +3,6 @@ class SHard
 {
    public static void main(String arg[])
    {
-   System.out.println("Welcome to new yes classes in Java and advance to classes point to noted");
+   System.out.println("Welcome to new yes classes in Java and advance to classes");
    }
 }
