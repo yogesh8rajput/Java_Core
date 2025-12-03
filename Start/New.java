70e525fc283a78995db9073a25e5132710e6bc90
yogesh8rajput <159117998+yogesh8rajput@users.noreply.github.com>
2025-12-03 21:12:59 +0530

    Update New.java

--- Start/New.java
+++ Start/New.java
@@ -2,7 +2,7 @@ class New
 {
     public static void main(String arg[])
     {
-        System.out.println("Hello New Day");
+        System.out.println("Hello New enjoy  Day");
         //  System.out.println('Hello New Day');
     }
 }
