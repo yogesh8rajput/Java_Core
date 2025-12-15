68ac1e10898d4f283e1a51f87574b591df6f6939
yogesh8rajput <yogeshmyrajput38203@gmail.com>
2025-12-15 21:37:30 +0530

    15 12 december

---
+++ Start/Neww.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Neww
+{
+    public static void main(String arg [])
+    {
+       int a;
+       int b;
+       Scanner sc=new Scanner(System.in);
+      
+      
+System.out.println("Type value if A:");
+ a=sc.nextInt();
+System.out.println("Type value if B:");
+ b=sc.nextInt();
+     if(a == b)
+     
+        System.out.println("equal");
+
+     
+     else if(a > b)
+     
+        System.out.println("A is greater");
+
+     
+     else
+     
+        System.out.println("B is greater");
+
+     
+   }
+       
+}
