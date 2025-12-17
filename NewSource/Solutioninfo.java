ec504bf0089fa5aeee1e114ae54566ae82ce0248
yogesh8rajput <yogeshmyrajput38203@gmail.com>
2025-12-17 21:55:43 +0530

    update

---
+++ NewSource/Solutioninfo.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solutioninfo {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+       
+        int i=1;
+       while( sc.hasNext()){
+        String s= sc.nextLine();
+        System.out.println(i+" "+s);
+        i++;
+       }
+    }
+}
