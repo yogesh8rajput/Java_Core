4c246560f21ad30968040cde16a5d275418cfe5d
yogesh8rajput <yogeshmyrajput38203@gmail.com>
2026-06-11 06:00:00 +0530

    Back 11

--- Latest/Conditional.java
+++ Latest/Conditional.java
@@ -10,14 +10,7 @@ public class Conditional {
         }else {    
         System.out.println(city+" This is ");    
         
-if(city == "Meerut") {    
-        System.out.println("city is meerut");    
-        }else if (city == "Noida") {    
-        System.out.println("city is noida");    
-        }else if(city == "Agra") {    
-        System.out.println("city is agra");    
-        }else {    
-        System.out.println(city+" This is "); 
+
         }    
     }    
 }    
