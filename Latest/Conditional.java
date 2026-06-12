8135f0435b1dc8299b1f9c6a9b146656db12af3c
yogesh8rajput <yogeshmyrajput38203@gmail.com>
2026-06-12 12:00:00 +0530

    Backdated commit for june 12

--- Latest/Conditional.java
+++ Latest/Conditional.java
@@ -9,6 +9,15 @@ public class Conditional {
         System.out.println("city is agra");    
         }else {    
         System.out.println(city+" This is ");    
+        
+if(city == "Meerut") {    
+        System.out.println("city is meerut");    
+        }else if (city == "Noida") {    
+        System.out.println("city is noida");    
+        }else if(city == "Agra") {    
+        System.out.println("city is agra");    
+        }else {    
+        System.out.println(city+" This is "); 
         }    
     }    
 }    
